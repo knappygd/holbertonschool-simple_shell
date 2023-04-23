e6ad2468555f4b85db16aad3adf859234bccad08
knappygd <emiliodamasco04@gmail.com>
2023-04-23 11:40:24 -0700

    exit status is now that of the command

--- errorhandler.c
+++ errorhandler.c
@@ -1,12 +1,11 @@
 #include "shell.h"
 
-char *err_constr(int flag, char *cmd, char *shellname)
+char *err_constr(int flag, int exit, char *cmd, char *shellname)
 {
     char *error = malloc(1024);
-    int exit_code = 1;
 
     if (flag == -1)
-        sprintf(error, "%s: %d: %s: not found", shellname, exit_code, cmd);
+        sprintf(error, "%s: %d: %s: not found", shellname, exit, cmd);
 
     return (error);
 }
