d8d3de87267957ddef1b3f2190f6731b73e37db6
knappygd <emiliodamasco04@gmail.com>
2023-04-23 17:23:02 -0700

    added error handling for exit & now also prints errors

--- errorhandler.c
+++ errorhandler.c
@@ -1,11 +1,17 @@
 #include "shell.h"
 
-char *err_constr(int flag, int exit, char *cmd, char *shellname)
+int err_constr(int flag, int exit, char **args, char *shellname)
 {
     char *error = malloc(1024);
+    char *cmd = args[0];
 
     if (flag == -1)
         sprintf(error, "%s: %d: %s: not found", shellname, exit, cmd);
+    else if (flag == -7)
+        sprintf(error, "%s: %d: %s: Illegal number: %s", shellname, exit, cmd, args[1]);
 
-    return (error);
+    printf("%s\n", error);
+    free(error);
+
+    return (0);
 }
