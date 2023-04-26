8b0c455489185d41946e847b9a4ea48d8fa78d7f
knapp <emiliodamasco04@gmail.com>
2023-04-26 05:52:07 -0700

    added env (here)

--- builtin.c
+++ builtin.c
@@ -16,38 +16,50 @@
  * change_dir - Changes to a newly specified working directory,
  * using the chdir() function.
  * @dir: A pointer to the directory to change.
+ *
+ * Return: The return value of the chdir() function.
  */
 int change_dir(char *dir)
 {
-    if (!dir)
-        dir = _getenv("HOME");
-    
-    int ret = chdir(dir);
+	if (!dir)
+		dir = _getenv("HOME");
 
-    if (ret == -1)
-    {
-        perror("chdir");
-        ret = 1;
-    }
-    return (ret);
+	int ret = chdir(dir);
+
+	if (ret == -1)
+	{
+		perror("chdir");
+		ret = 1;
+	}
+	return (ret);
 }
 
+/**
+ * exit_sh - Exits the shell. If no argument is passed, it exits with 0.
+ * Otherwise, it converts the argument to an int and exits with that number.
+ * @exit_c: The exit code.
+*/
 void exit_sh(int exit_c)
 {
-    int ret;
+	int ret;
 
-    if (!exit_c)
-        exit_c = 0;
+	if (!exit_c)
+		exit_c = 0;
 
-    exit(exit_c);
+	exit(exit_c);
 }
 
+/**
+ * print_env - Prints the contents of the environ variable in newlines.
+ *
+ * Return: 0
+*/
 int print_env(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; environ[i]; i++)
-        printf("%s\n", environ[i]);
+	for (i = 0; environ[i]; i++)
+		printf("%s\n", environ[i]);
 
-    return (0);
+	return (0);
 }
