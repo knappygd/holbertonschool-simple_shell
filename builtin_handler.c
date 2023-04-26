bfb77f986206950bd8e3d272e98b9372b8c3c4e2
knapp <emiliodamasco04@gmail.com>
2023-04-26 05:51:25 -0700

    added docs

--- builtin_handler.c
+++ builtin_handler.c
@@ -1,52 +1,76 @@
 #include "shell.h"
+/*
+ * This file contains builtin_handler(), responsible for managing a
+ * built-in command input.
+ * Depending on the built-in command index, it will execute the
+ * corresponding one.
+ *
+ * 1: cd
+ * 2: exit
+ * 3: env
+*/
 
+/**
+ * builtin_handler - Decides what command to execute based on its index.
+ * @args: The command arguments array.
+ * @shellname: The argv[0] string.
+ * @index: The command index.
+ *
+ * Return: The exit value (0 if successful, 1 if not).
+*/
 int builtin_handler(char **args, char *shellname, int index)
 {
-    int exit = 0;
+	int exit = 0;
 
-    switch (index)
-    {
-    case 1:
-        if (!args[1])
-            exit = change_dir(_getenv("HOME"));
-        else
-            exit = change_dir(args[1]);
-        if (exit == 1)
-                err_constr(-8, exit, args, shellname);
-        break;
-    case 2:
-        if (!args[1])
-            exit_sh(0);
+	switch (index)
+	{
+	case 1:
+		if (!args[1])
+			exit = change_dir(_getenv("HOME"));
+		else
+			exit = change_dir(args[1]);
+		if (exit == 1)
+			err_constr(-8, exit, args, shellname);
+		break;
+	case 2:
+		if (!args[1])
+			exit_sh(0);
 
-        else
-        {
-            if (atoi4exit(args[1]) == -1)
-            {
-                exit = 1;
-                err_constr(-7, exit, args, shellname);
-            }
+		else
+		{
+			if (atoi4exit(args[1]) == -1)
+			{
+				exit = 1;
+				err_constr(-7, exit, args, shellname);
+			}
 
-            else
-                exit_sh(atoi4exit(args[1]));
-        }
-        case 3:
-        exit = print_env();
-        break;
-    default:
-        break;
-    }
+			else
+				exit_sh(atoi4exit(args[1]));
+		}
+	case 3:
+		exit = print_env();
+		break;
+	default:
+		break;
+	}
 
-    return (exit);
+	return (exit);
 }
 
+/**
+ * is_builtin - Checks if the command passed is a built-in command.
+ * @cmd: The command to check.
+ *
+ * Return: The index of the command, or -1 if the command is not a built-in.
+*/
 int is_builtin(char *cmd)
 {
-    char *builtin_arr[] = {"cd", "exit", "env", NULL};
-    int i;
+	char *builtin_arr[] = {"cd", "exit", "env", NULL};
+	int i;
 
-    for (i = 0; builtin_arr[i]; i++)
-        if (_strcmp(cmd, builtin_arr[i]) == 0)
-            return (i + 1);
+	for (i = 0; builtin_arr[i]; i++)
+		if (_strcmp(cmd, builtin_arr[i]) == 0)
+			return (i + 1);
 
-    return (-1);
+	return (-1);
 }
